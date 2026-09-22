339362c873db3ca1205882a9c66c91feb16c651c
Fahad Muneer <fahadmuneer843@gmail.com>
2026-09-22 07:44:36 -0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menuToggle");
+    const navMenu = document.querySelector(".nav-menu");
+
+    if (menuToggle && navMenu) {
+        menuToggle.addEventListener("click", () => {
+            navMenu.classList.toggle("show");
+        });
+    }
+});
